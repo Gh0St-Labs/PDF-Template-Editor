84314c0889bcfc9ba4682cb6d3ceda4430d7a6fb
Gh0St_Labs <kordan.goat@gmail.com>
2023-03-21 11:13:30 +0530

    Final Setup 2

--- main.py
+++ main.py
@@ -16,27 +16,31 @@ for index, row in data.iterrows():
     pdf.cell(w=0, h=0, txt=row['Topic'], align="L", ln=1)
     pdf.cell(w=0, h=0, txt="  /  /  ", align="R")
     pdf.line(10,20, 200, 20)
-    for i in range(20, 290, 10):
-        pdf.line(10, i, 200, i)
 
     pdf.ln(270)
     pdf.set_font(family='Times', style="I", size=8)
     pdf.set_text_color(91, 77, 77)
     pdf.cell(w=0, h=8, txt=row['Topic'], align="R")
+
+    pdf.image(x=10, y=281, w=5, h=5,
+        name='kisspng-python-computer-icons-tutorial-computer-programmin-social-icons-5ad5ccbb30c4a8.2707803315239610191998.jpg',
+              link='https://www.python.org/')
     for int in range(row['Pages'] - 1):
         pdf.add_page()
         pdf.set_font(family='Times', style="B", size=30)
         pdf.set_text_color(62, 52, 206)
         pdf.cell(w=0, h=0, txt="  /  /  ", align="R")
         pdf.line(10, 20, 200, 20)
+        pdf.image(x=10, y=281, w=5, h=5,
+                  name='kisspng-python-computer-icons-tutorial-computer-programmin-social-icons-5ad5ccbb30c4a8.2707803315239610191998.jpg',
+                  link='https://www.python.org/')
 
         pdf.ln(270)
 
         pdf.set_font(family='Times', style="I", size=8)
         pdf.set_text_color(91, 77, 77)
         pdf.cell(w=0, h=8, txt=row['Topic'], align="R")
-        for i in range(20, 290, 10):
-            pdf.line(10, i, 200, i)
+
 
 
 
